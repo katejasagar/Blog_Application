d283e9d3e12378c41633e014e23489daa029a8f1
Sagar Kateja <katejasagar@gmail.com>
2021-05-31 01:34:50 +0530

    created user app to register user/ added validations in the form / added crispy form

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import redirect, render
+#from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Accont created for {username}!')
+            return redirect('Blog-Home')
+    else: 
+        form = UserRegisterForm()
+    return render(request, "users/register.html", {'form': form})
